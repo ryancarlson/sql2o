56618fb6d35f3081d2e3028032bdce2e3014c679
ryancarlson <ryan.t.carlson@cru.org>
2014-07-21 12:00:00 -0400

    change timing log entries from info to debug for cleaner logs

--- src/main/java/org/sql2o/Query.java
+++ src/main/java/org/sql2o/Query.java
@@ -361,7 +361,7 @@ public class Query {
             rs.close();
             long afterClose = System.currentTimeMillis();
 
-            logger.info("total: {} ms, execution: {} ms, reading and parsing: {} ms; executed [{}]", new Object[]{
+            logger.debug("total: {} ms, execution: {} ms, reading and parsing: {} ms; executed [{}]", new Object[]{
                     afterClose - start, 
                     afterExecQuery-start, 
                     afterClose - afterExecQuery, 
@@ -397,7 +397,7 @@ public class Query {
             Table table = TableFactory.createTable(rs, this.isCaseSensitive(), this.connection.getSql2o().quirksMode);
             long afterClose = System.currentTimeMillis();
             
-            logger.info("total: {} ms, execution: {} ms, reading and parsing: {} ms; executed fetch table [{}]", new Object[]{
+            logger.debug("total: {} ms, execution: {} ms, reading and parsing: {} ms; executed fetch table [{}]", new Object[]{
                 afterClose - start, 
                 afterExecute-start, 
                 afterClose - afterExecute, 
@@ -429,7 +429,7 @@ public class Query {
         }
 
         long end = System.currentTimeMillis();
-        logger.info("total: {} ms; executed update [{}]", new Object[]{
+        logger.debug("total: {} ms; executed update [{}]", new Object[]{
             end - start, 
             this.getName() == null ? "No name" : this.getName()
         });
@@ -444,7 +444,7 @@ public class Query {
             if (rs.next()){
                 Object o = getRSVal(rs, 1);
                 long end = System.currentTimeMillis();
-                logger.info("total: {} ms; executed scalar [{}]", new Object[]{
+                logger.debug("total: {} ms; executed scalar [{}]", new Object[]{
                     end - start, 
                     this.getName() == null ? "No name" : this.getName()
                 });
@@ -487,7 +487,7 @@ public class Query {
             }
 
             long end = System.currentTimeMillis();
-            logger.info("total: {} ms; executed scalar list [{}]", new Object[]{
+            logger.debug("total: {} ms; executed scalar list [{}]", new Object[]{
                 end - start,
                 this.getName() == null ? "No name" : this.getName()
             });
@@ -529,7 +529,7 @@ public class Query {
         }
 
         long end = System.currentTimeMillis();
-        logger.info("total: {} ms; executed batch [{}]", new Object[]{
+        logger.debug("total: {} ms; executed batch [{}]", new Object[]{
             end - start,
             this.getName() == null ? "No name" : this.getName()
         });
