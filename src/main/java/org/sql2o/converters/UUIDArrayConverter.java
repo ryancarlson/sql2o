5274638e8433d7b4dfc8d42c61d2178501c9be87
Lee Braddock <lee.braddock@cru.org>
2014-07-29 15:17:16 -0400

    Convert Jdbc4Array of uuid's to UUID[].

--- src/main/java/org/sql2o/converters/UUIDArrayConverter.java
+++ src/main/java/org/sql2o/converters/UUIDArrayConverter.java
@@ -1,22 +1,54 @@
 package org.sql2o.converters;
 
+import org.postgresql.jdbc4.Jdbc4Array;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.util.UUID;
 
 /**
  * Used by sql2o to convert a value from the database into a {@link java.util.UUID}.
  */
-public class UUIDArrayConverter implements Converter<UUID[]> {
+public class UUIDArrayConverter implements Converter<UUID[]>
+{
+	public UUID[] convert(Object val) throws ConverterException
+	{
+		if (val == null)
+		{
+			return null;
+		}
 
-    public UUID[] convert(Object val) throws ConverterException {
-        if (val == null){
-            return null;
-        }
+		if (Jdbc4Array.class.isAssignableFrom(val.getClass()))
+		{
+			Jdbc4Array jdbc4Array = (Jdbc4Array) val;
 
-        if (UUID[].class.isAssignableFrom( val.getClass() )){
-            return (UUID[])val;
-        }
+			try
+			{
+				ResultSet resultSet = jdbc4Array.getResultSet();
 
-        throw new ConverterException("Cannot convert type " + val.getClass().toString() + " to java.util.UUID[]");
-    }
+				// get result set row count
+				resultSet.last();
+				UUID[] uuids = new UUID[resultSet.getRow()];
+				resultSet.beforeFirst();
+
+				// get uuid from last column in each row
+				for (int i = 0; resultSet.next(); i++)
+				{
+					ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+					String result = resultSet.getString(resultSetMetaData.getColumnCount());
+					uuids[i] = UUID.fromString(result);
+				}
+
+				return uuids;
+			}
+			catch (SQLException e)
+			{
+				throw new ConverterException("Cannot convert type " + val.getClass().toString() + " to java.util.UUID[] due to sql exception", e);
+			}
+		}
+
+		throw new ConverterException("Cannot convert type " + val.getClass().toString() + " to java.util.UUID[]");
+	}
 }
 
